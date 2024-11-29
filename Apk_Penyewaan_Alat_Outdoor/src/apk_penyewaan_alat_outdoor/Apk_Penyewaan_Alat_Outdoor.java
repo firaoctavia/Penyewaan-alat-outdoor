cbfe9cf1594ff661279c3a33f8bbe324dca368bb
firaoctavia <email.firaoctavia@gmail.com>
2024-11-29 12:04:32 +0700

    Add files via upload

---
+++ Apk_Penyewaan_Alat_Outdoor/src/apk_penyewaan_alat_outdoor/Apk_Penyewaan_Alat_Outdoor.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package apk_penyewaan_alat_outdoor;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Apk_Penyewaan_Alat_Outdoor {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
